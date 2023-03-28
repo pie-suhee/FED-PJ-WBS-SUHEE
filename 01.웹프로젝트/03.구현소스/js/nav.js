c2f702085454303261c1bc7069ac4f585ccd4573
pie-suhee <tngmltngml4@naver.com>
2023-03-28 10:35:40 +0900

    연습

---
+++ 01.웹프로젝트/03.구현소스/js/nav.js
@@ -0,0 +1,18 @@
+const nav = {
+    "lnb" : {
+        "COMPANY" : {
+            "link" : "#",
+            "bnb" : ["INTRODUCTION", "HISTORY", " CONTACT US"],
+        },
+        "ARTIST" : {
+            "link" : "#",
+            "bnb" : ["ARTIST"],
+        },
+        "COMMUNITY" : {
+            "link" : "#",
+            "bnb" : ["NOTICE", "NEWS"],
+        },
+    }
+}
+
+export default nav;
