c8e671c8b709088a0bf8c2b23e4aa1dabc939f92
Tom B. Parker <tombap8@hotmail.com>
2023-03-28 12:22:54 +0900

    ss

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -98,48 +98,86 @@ const total = cards.length;
 const slice = 2 * Math.PI / total;
 let scrollProgress = 0;
 
-function setup() {
-  const radius = 1500;
-  const center = 1500;
+// function setup() {
+//   const radius = 1500;
+//   const center = 1500;
 
-  cards.forEach((card, index) => {
-    const angle = index * slice;
-    const inverseAngle = (index + total / 2) % total * slice;
-    const x = center + radius * Math.sin(angle);
-    const y = center - radius * Math.cos(angle);
-    const firstX = center + radius * Math.sin(0);
-    const inverseY = (center - radius * Math.cos(inverseAngle)) / 2;
+//   cards.forEach((card, index) => {
+//     const angle = index * slice;
+//     const inverseAngle = (index + total / 2) % total * slice;
+//     const x = center + radius * Math.sin(angle);
+//     const y = center - radius * Math.cos(angle);
+//     const firstX = center + radius * Math.sin(0);
+//     const inverseY = (center - radius * Math.cos(inverseAngle)) / 2;
 
-    card.style.transform = `translate(calc(${x - firstX}px - 50%), calc(${y - inverseY}px - 50%)) rotate(-${angle}rad)`;
-    card.querySelector("img").style.transform = `rotate(${angle * 2}rad)`;
-  });
-}
+//     card.style.transform = `translate(calc(${x - firstX}px - 50%), calc(${y - inverseY}px - 50%)) rotate(-${angle}rad)`;
+//     card.querySelector("img").style.transform = `rotate(${angle * 2}rad)`;
+//   });
+// }
 
-function updateCards() {
-  /* const radius = wheel.offsetWidth / 2;
-  const center = wheel.offsetWidth / 2; */
-  const radius = 1500;
-  const center = 1500;
-  const scrollPosition = window.scrollY;
-  scrollProgress = scrollPosition / (document.body.offsetHeight - window.innerHeight);
-  
-  cards.forEach((card, index) => {
-    if((index - scrollProgress * total) < 0){
-      index += total;
-    }
-    let angle = (index - scrollProgress * total) * slice; 
-    const inverseAngle = (index + total / 2 - scrollProgress * total) % total * slice;
-    const x = center + radius * Math.sin(angle);
-    const y = center - radius * Math.cos(angle);
-    const firstX = center + radius * Math.sin(0);
-    const inverseY = (center - radius * Math.cos(inverseAngle)) / 2;
-    
-    card.style.transform = `translate(calc(${x - firstX}px - 50%), calc(${y - inverseY}px - 50%)) rotate(-${angle}rad)`;
-    card.querySelector("img").style.transform = `rotate(${angle * 2}rad)`;
-  });
+function setup(){
+  cards.forEach((ele,idx)=>{
+    ele.style.transform = `rotate(${idx*20}deg) translateY(-450%)`;
+  })
 }
 
 setup();
 
-window.addEventListener("scroll", updateCards);
+let protUp = 0;
+let angle = 360/32*1.76; 
+let deg = 0;
+
+function updateCards() {
+  if(protUp) return;
+  protUp = 1;
+  setTimeout(() => {
+    protUp=0;
+  }, 300);
+
+  let dir = event.wheelDelta;
+  
+  if(dir < 0)  deg = deg - angle;
+  else deg = deg + angle;
+
+  if(deg<-360) deg=-360;
+  else if(deg>0) deg=0;
+
+  console.log(dir,deg);
+  
+  wheel.style.transform = `translateX(-50%) rotate(${deg}deg)`;
+
+   
+}
+// function updateCards() {
+//   if(protUp) return;
+//   protUp = 1;
+//   setTimeout(() => {
+//     protUp=0;
+//   }, 500);
+//   /* const radius = wheel.offsetWidth / 2;
+//   const center = wheel.offsetWidth / 2; */
+//   const radius = 500;
+//   const center = 1500;
+//   const scrollPosition = window.scrollY;
+//   scrollProgress = scrollPosition / (document.body.offsetHeight - window.innerHeight);
+  
+//   cards.forEach((card, index) => {
+//     if((index - scrollProgress * total) < 0){
+//       index += total;
+//     }
+//     let angle = (index - scrollProgress * total) * slice; 
+//     const inverseAngle = (index + total / 2 - scrollProgress * total) % total * slice;
+//     const x = center + radius * Math.sin(angle);
+//     const y = center - radius * Math.cos(angle);
+//     const firstX = center + radius * Math.sin(0);
+//     const inverseY = (center - radius * Math.cos(inverseAngle)) / 2;
+    
+//     card.style.transform = `translate(calc(${x - firstX}px - 50%), calc(${y - inverseY}px - 50%)) rotate(-${angle}rad)`;
+//     card.querySelector("img").style.transform = `rotate(${angle * 2}rad)`;
+//   });
+// }
+
+// setup();
+
+window.addEventListener("wheel", updateCards);
 /* img_con 배치 & scroll 이벤트 끝 */
