9ff7e929e6ffbe27b93027b51c2ea99bdb95834c
pie-suhee <tngmltngml4@naver.com>
2023-03-29 13:02:53 +0900

    연습

--- 01.웹프로젝트/03.구현소스/js/company.js
+++ 01.웹프로젝트/03.구현소스/js/company.js
@@ -27,3 +27,47 @@ for(let i in nav.lnb){
 lnb.appendChild(mbg);
 lnb.appendChild(navUl);
 /* nav html 동적 생성 끝 */
+
+/* poster 슬라이드 시작 */
+$(document).ready(function () {
+  let posterSwiper = new Swiper(".posterSwiper", {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      formatFractionCurrent: function (number) {
+        return ('0' + number).slice(-2);
+      },
+      formatFractionTotal: function (number) {
+        return ('0' + number).slice(-2);
+      },
+      renderFraction: function (currentClass, totalClass) {
+        return '<span class="' + currentClass + '"></span>' +
+             '<span class="' + totalClass + '"></span>';
+      }
+    },
+    speed: 500,
+    loop: true,
+    on: {
+      init: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionEnd: function () {
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+      }
+    }
+  });
+});
+/* poster 슬라이드 끝 */
