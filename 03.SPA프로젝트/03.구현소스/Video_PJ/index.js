478dd9c113d871a6b022dbae536de844228001a7
pie-suhee <tngmltngml4@naver.com>
2023-06-26 16:12:24 +0900

    20230626

--- 03.SPA프로젝트/03.구현소스/Video_PJ/index.js
+++ 03.SPA프로젝트/03.구현소스/Video_PJ/index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:admin123@videocluster.xwhx5hw.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', async (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  await user.save().then(()=>{
+    res.status(200).json({success:true,}).catch((err)=>{
+      console.log(err)
+      res.json({
+        success: false,
+        err: err,
+      })
+    })
+  })
 })
 
 app.listen(port, () => {
