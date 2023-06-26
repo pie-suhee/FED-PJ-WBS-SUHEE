753075439de20468d1e330998db533e57483b10e
pie-suhee <tngmltngml4@naver.com>
2023-06-26 16:21:54 +0900

    20230626

--- 03.SPA프로젝트/03.구현소스/Video_PJ/index.js
+++ 03.SPA프로젝트/03.구현소스/Video_PJ/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
 const { User } = require('./models/User')
 
 // application/x-www-form-urlencoded
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin123@videocluster.xwhx5hw.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
