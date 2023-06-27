2744b83235ddc319f80815ea5aae3a4b815c9081
pie-suhee <tngmltngml4@naver.com>
2023-06-27 12:53:52 +0900

    20230627

---
+++ 03.SPA프로젝트/03.구현소스/Boiler_PJ/client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
